8ad0de82f48d1901e0190b9fe94c14e33b3d42d5
GuilhermeRodriguesMadeinweb <guilherme.rodrigues@madeinweb.app>
2024-06-18 22:09:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+// Query params = nome=NodeJs
+// Route params = /curso/2
+// Request body = { nome: 'NodeJs', tipo: 'Backend' }
+
+// CRUD: Create, Read, Update, Delete
+
+const cursos = ["Node JS", "JavaScript", "React Native"];
+
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+
+server.get("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
+});
+
+server.post("/cursos", (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+});
+
+server.delete("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
+  return res.json({ message: "Curso deletado com sucesso!" });
+});
+
+server.listen(3000);
